ccf42889e45ce8b2b13a0e0d85ec7adc20b45716
Evrad7 <tsoataevrad7@gmail.com>
2025-10-23 08:41:37 +0200

    configure google analytics and automation for ssl certification with cerbot on elastic beanstalk

--- Template.js
+++ Template.js
@@ -19,7 +19,7 @@ const  Template=({children})=>{
         <link rel="icon" href={require("./client/src/assets/img/favicon.png")} />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"  />
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Web site created using create-react-app"/>
+        <meta name="description" content="Votre agence web de développement web & mobile."/>
         {/* <link rel="apple-touch-icon" href="logo192.png" /> */}
         {/* <link rel="manifest" href="/manifest.json" /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -28,7 +28,9 @@ const  Template=({children})=>{
         <link href="https://fonts.cdnfonts.com/css/lucida-handwriting-std" rel="stylesheet" />
         <title>DEVTE</title>
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-DCHN40J1WL"></script>
-      <script>{googleAnalytic()}</script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-DCHN40J1WL"></script>
+      <script dangerouslySetInnerHTML={{__html:"window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());\gtag('config', 'G-DCHN40J1WL');"}}>
+      </script>
       </head>
       <body 
       >
